68fadbdfd72c596913fb7c4e226d5c9ff3751f4a
Kevin Stewart <kistewart419@gmail.com>
2020-02-24 23:06:14 -0500

    Add Xcode project

---
+++ GitKraken/GitKraken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitKraken
+//
+//  Created by Kevin Stewart on 2/24/20.
+//  Copyright © 2020 Kevin Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
